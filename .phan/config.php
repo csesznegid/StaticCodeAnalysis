ac8704978950d024887b7a5d355e72c2af1416c5
Denes Csesznegi <denes.csesznegi@codecool.com>
2020-09-23 21:22:24 +0200

    style: use single quotes inside the phan configuration file

--- .phan/config.php
+++ .phan/config.php
@@ -1,12 +1,12 @@
 <?php
 
 return array(
-    "target_php_version" => null,
+    'target_php_version' => null,
     'directory_list' => array(
         'app',
         'src',
     ),
-    "exclude_analysis_directory_list" => array(
+    'exclude_analysis_directory_list' => array(
         'vendor',
     ),
     'plugins' => array(),
