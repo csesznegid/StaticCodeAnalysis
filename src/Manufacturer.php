ec1badfaa3be23303242a80229c3211ed483b9ae
Denes Csesznegi <denes.csesznegi@codecool.com>
2020-09-23 21:28:47 +0200

    docs: add phpdoc comments to manufacturer class

--- src/Manufacturer.php
+++ src/Manufacturer.php
@@ -1,11 +1,36 @@
 <?php
 
+/**
+ * This file is part of the "Static Code Analysis" package
+ *
+ * @author Denes Csesznegi
+ * @since  2020.09.23.
+ */
+
 namespace StaticCodeAnalysis;
 
+/**
+ * Class Manufacturer
+ *
+ * @package StaticCodeAnalysis
+ */
 class Manufacturer
 {
+    /**
+     * Unique ID
+     *
+     * @var    string $identifier
+     * @access private
+     */
     private $identifier;
 
+    /**
+     * Available Manufacturers' list
+     *
+     * @var    string[] $availableManufacturers
+     * @access private
+     * @static
+     */
     private static $availableManufacturers = array(
         'Apple'           => 'Electronics',
         'Nestle'          => 'Food & Beverages',
@@ -13,6 +38,13 @@ class Manufacturer
         'Lockheed Martin' => 'Aerospace & Defense',
     );
 
+    /**
+     * Manufacturer constructor
+     *
+     * @param  null|string $identifier
+     * @throws \Exception
+     * @access public
+     */
     public function __construct(?string $identifier = null)
     {
         $time = time();
@@ -23,21 +55,45 @@ class Manufacturer
         $this->identifier = $identifier ?? uniqid();
     }
 
+    /**
+     * Returns unique ID
+     *
+     * @return string
+     * @access public
+     */
     public function getIdentifier()
     {
         return $this->identifier;
     }
 
+    /**
+     * Returns the keys (names) of the available manufacturers
+     *
+     * @return string[]
+     * @access public
+     */
     public function getNames()
     {
         return array_keys(self::$availableManufacturers);
     }
 
+    /**
+     * Returns the values (industries) of the available manufacturers
+     *
+     * @return string[]
+     * @access public
+     */
     public function getIndustries()
     {
         return array_values(self::$availableManufacturers);
     }
 
+    /**
+     * Returns the available manufacturers
+     *
+     * @return string[]
+     * @access public
+     */
     public function getAvailableManufacturers()
     {
         return self::$availableManufacturers;
