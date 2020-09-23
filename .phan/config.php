70d5cde8db9f86a6034a1bc83d40e720efacb994
Denes Csesznegi <denes.csesznegi@codecool.com>
2020-09-23 14:44:33 +0200

    chore: add phan configuration file

---
+++ .phan/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    "target_php_version" => null,
+    'directory_list' => array(
+        'app',
+        'src',
+    ),
+    "exclude_analysis_directory_list" => array(
+        'vendor',
+    ),
+    'plugins' => array(),
+);
